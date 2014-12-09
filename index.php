39d7a1697e2a21395f002348a9df6d9bdca6283d
jvisker <jamievisker@gmail.com>
2014-12-08 19:04:06 -0700

    h2

--- index.php
+++ index.php
@@ -519,7 +519,7 @@ while($row = mysql_fetch_array($sql)) {
 			</div>
 			<form action="" name="upload_by_client" id="upload_by_client" method="post" enctype="multipart/form-data">
 						<div style = "text-align: center">
-						<h3>Hashtag</h3>
+						<h2>Hashtag</h2>
 				<input type="text" name="hashtag" id="hashtag2"  value="" />
 				
 				</div>
