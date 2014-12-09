ba140a22f85846aa16dd742f3388b837d48043e2
jvisker <jamievisker@gmail.com>
2014-12-08 20:12:12 -0700

    commit

--- index.php
+++ index.php
@@ -521,12 +521,12 @@ while($row = mysql_fetch_array($sql)) {
 						<div style = "text-align: center">
 						
 						
-						<img src="http://shpaste.elasticbeanstalk.com/img/custom/logo/test.jpg" width="120%" height = "120%">
+						<img src="http://shpaste.elasticbeanstalk.com/img/custom/logo/test.jpg" width="100%" height = "100%">
 				<br>
 				<br>
 				<br>
 				<br>
-				<input type="text" name="hashtag" id="hashtag2" style="width:1500px" value="" />
+				<input type="text" name="hashtag" id="hashtag2" style="width:800px" value="" />
 				
 				</div>
 				
