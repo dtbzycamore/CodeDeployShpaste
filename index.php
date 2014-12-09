7d170ee239af85553ae6b61acc06fd7acd65afc9
jvisker <jamievisker@gmail.com>
2014-12-08 22:53:21 -0700

    check2

--- index.php
+++ index.php
@@ -448,7 +448,14 @@ while($row = mysql_fetch_array($sql)) {
 							
 						
 						}
-				
+					if (document.getElementById('hashtag2').value == '') {
+						document.getElementById("uploader").style.display = 'none';
+						document.getElementById("btn-submit").style.display  = 'none';
+						}
+						else{
+						document.getElementById("uploader").style.display = 'none';
+						document.getElementById("btn-submit").style.display  = '';
+						}
 						
 						
 						
